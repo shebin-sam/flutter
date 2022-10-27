9b6ae87ef2db0ad9fd982f9d9398d2c00a6fad8d
shebin-sam <shebinsam22@karunya.edu.in>
2022-10-27 21:26:42 +0530

    flutter learning 3

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -6,16 +6,11 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(child: Text("Login Page",style: TextStyle(
-        fontSize: 40,
-        color: Colors.blue,
-        fontWeight: FontWeight.bold
+      child: Column(
+        children: [Image.asset("assets/images/login_image.png",fit: BoxFit.cover,)],
+      )
       
-      ),
-      ),
-      ),
-      
-
-    );
+      );
+  
   }
 }
